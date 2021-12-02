0247c7209b9520a4201a54a039eaf110ab16da90
Bruno <brunomorello7@gmail.com>
2021-12-02 19:11:12 -0300

    initialize repository

---
+++ app/src/main/java/br/com/bmo/mob/list4u/ui/activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package br.com.bmo.mob.list4u.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.bmo.mob.list4u.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
